f76bea384dbe7ee00771050b8919b9e90c2b081c
joyahmed <joy_ahmed_007@yahoo.com>
2021-09-02 21:01:55 +0600

    Add project files.

---
+++ RestaurantAPI/Models/OrderMaster.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        [Column(TypeName = "nvarchar(10)")]
+        public string PaymentMethod { get; set; }
+        public decimal GrandTotal { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
